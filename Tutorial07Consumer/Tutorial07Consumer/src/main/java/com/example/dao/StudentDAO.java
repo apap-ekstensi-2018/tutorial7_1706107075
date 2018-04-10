bae65c673abdc9b9959b6ed80c01423798af58ec
Winda Dumaria S <winda.dumaria@ruma.co.id>
2018-04-10 23:46:59 +0700

    consume services

---
+++ Tutorial07Consumer/Tutorial07Consumer/src/main/java/com/example/dao/StudentDAO.java
@@ -0,0 +1,12 @@
+package com.example.dao;
+
+import java.util.List;
+
+import com.example.model.StudentModel;
+
+public interface StudentDAO {
+	
+	StudentModel selectStudent (String npm);
+	List<StudentModel> selectAllStudents();
+
+}
---
+++ Tutorial07Consumer/Tutorial07Consumer/src/main/java/com/example/dao/StudentDAOImpl.java
@@ -0,0 +1,30 @@
+package com.example.dao;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.model.StudentModel;
+
+@Service
+public class StudentDAOImpl implements StudentDAO {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public StudentModel selectStudent(String npm) {
+		StudentModel student = restTemplate.getForObject("http://localhost:8080/rest/student/view/"+npm, StudentModel.class);
+		return student;
+	}
+
+	@Override
+	public List<StudentModel> selectAllStudents() {
+		ResponseEntity <StudentModel[]> response = restTemplate.getForEntity("http://localhost:8080/rest/student/viewall", StudentModel[].class);
+		return Arrays.asList(response.getBody());
+	}
+
+}
